686914f028e0c35499f9bca4cdf815ff8b5b4c15
Фёдор Сорокин <sorokinfedor715@gmail.com>
2026-01-22 10:54:08 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -10,4 +10,5 @@ export const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  databaseURL: "https://whitegram-16506-pd-default-rtdb.firebaseio.com",
 };
