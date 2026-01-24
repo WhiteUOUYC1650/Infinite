d0db4b9167979827ba62d56323868fcd2886225d
Фёдор Сорокин <sorokinfedor715@gmail.com>
2026-01-24 21:16:20 +0000

    Does not work... Then make sure that the dialogs are in full screen mode

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -38,8 +38,14 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=closed]:pointer-events-none sm:rounded-lg",
-        "max-h-[90svh] overflow-y-auto",
+        // Base: mobile fullscreen
+        "fixed z-50 grid w-full h-full top-0 left-0 gap-4 border-0 bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:pointer-events-none",
+        // Desktop (sm and up)
+        "sm:h-auto sm:w-full sm:max-w-lg sm:left-[50%] sm:top-[50%] sm:translate-x-[-50%] sm:translate-y-[-50%] sm:border sm:rounded-lg sm:max-h-[90svh]",
+        // Desktop exit animation
+        "sm:data-[state=closed]:slide-out-to-left-1/2 sm:data-[state=closed]:slide-out-to-top-[48%]",
+        // Common
+        "overflow-y-auto",
         className
       )}
       {...props}
