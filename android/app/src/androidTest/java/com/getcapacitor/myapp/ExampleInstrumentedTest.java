f97db8e3583f7f45468ca9f9dc1de935ee8941cb
Фёдор Сорокин <sorokinfedor715@gmail.com>
2026-01-25 11:13:28 +0000

    It didn't work anyway. I think github understands that the placeholder i

--- android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
@@ -1,4 +1,19 @@
 package com.getcapacitor.myapp;
 
-// This file is intentionally left blank to resolve build issues.
-public class ExampleInstrumentedTest {}
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ * This file is intentionally minimal to solve build issues.
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // A dummy test to ensure the class is valid.
+        assertEquals(4, 2 + 2);
+    }
+}
--- android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
+++ android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
@@ -1,4 +1,15 @@
 package com.getcapacitor.myapp;
 
-// This file is intentionally left blank to resolve build issues.
-public class ExampleUnitTest {}
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ * This file is intentionally minimal to solve build issues.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
