6c5b501f3eabee40b4eb78b2795e92d59a946949
Фёдор Сорокин <sorokinfedor715@gmail.com>
2026-01-27 12:25:50 +0000

    Why do you need a new Firebase project again? If I let it, everything wi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,11 +4,11 @@
 // https://firebase.google.com/docs/web/setup#get-config
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: "https://whitegram-16506-pd-default-rtdb.firebaseio.com",
+  apiKey: "AIzaSyA_...real-key-placeholder",
+  authDomain: "infinite-chat.firebaseapp.com",
+  projectId: "infinite-chat",
+  storageBucket: "infinite-chat.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:1234567890abcdef",
+  databaseURL: "https://infinite-chat.firebaseio.com",
 };
