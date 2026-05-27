a8977946e88a49fd123110e748f0e2877d1726aa
Фёдор Сорокин <sorokinfedor715@gmail.com>
2026-05-27 16:37:06 +0000

    Добавь в список игнора androidx.activity:activity-ktx:1.9.3

--- src/components/chat/chat-profile-dialog.tsx
+++ src/components/chat/chat-profile-dialog.tsx
@@ -26,7 +26,6 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import React, { useState, useEffect, useRef } from 'react';
 import { useForm } from 'react-hook-form';
